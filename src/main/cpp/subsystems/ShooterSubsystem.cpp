757f644e74f50598e316fba9295206d0a2a9ccde
Scott Coursin <scott.coursin@gmail.com>
2024-02-05 20:32:58 -0600

    Filled in the Shooter Subsystem

--- src/main/cpp/subsystems/ShooterSubsystem.cpp
+++ src/main/cpp/subsystems/ShooterSubsystem.cpp
@@ -1,13 +1,66 @@
 #include "subsystems/ShooterSubsystem.h"
 
 ShooterSubsystem::ShooterSubsystem()
+  : m_OverWheels(12, rev::CANSparkLowLevel::MotorType::kBrushless)
+  , m_UnderWheels(13, rev::CANSparkLowLevel::MotorType::kBrushless)
+  , m_BackWheels(1, rev::CANSparkLowLevel::MotorType::kBrushless)
+  , m_ElevationController(2, rev::CANSparkLowLevel::MotorType::kBrushless)
+  , m_ElevationEncoder(1)
 {
   wpi::log::DataLog& log = frc::DataLogManager::GetLog();
 
-  m_log = wpi::log::DoubleLogEntry(log, "/subsystem/shooter");
+  m_logOverRPM = wpi::log::DoubleLogEntry(log, "/subsystem/shooter/OverRPM");
+  m_logUnderRPM = wpi::log::DoubleLogEntry(log, "/subsystem/shooter/UnderRPM");
+  m_logBackRPM = wpi::log::DoubleLogEntry(log, "/subsystem/shooter/BackRPM");
+  m_logCurrentAngle = wpi::log::DoubleLogEntry(log, "/subsystem/shooter/CurrentAngle");
+  m_logCommandedAngle = wpi::log::DoubleLogEntry(log, "/subsystem/shooter/CommandedAngle");
+  m_logAbsoluteAngle = wpi::log::DoubleLogEntry(log, "/subsystem/shooter/AbsoluteAngle");
+
+  auto result = m_ElevationEncoder.GetAbsolutePosition();
+  m_ElevationRelativeEnc.SetPosition(result.GetValueAsDouble());
 }
 
 void ShooterSubsystem::Periodic()
 {
   // m_log.Append(add_data_here);
+  m_logOverRPM.Append(m_OverRelativeEnc.GetVelocity()); 
+  m_logUnderRPM.Append(m_UnderRelativeEnc.GetVelocity());
+  m_logBackRPM.Append(m_BackRelativeEnc.GetVelocity());
+  m_logCurrentAngle.Append(m_ElevationRelativeEnc.GetPosition());
+  m_logCommandedAngle.Append(m_elevationAngle);
+  m_logAbsoluteAngle.Append(m_ElevationEncoder.GetAbsolutePosition().GetValueAsDouble());
+
 }
+
+void ShooterSubsystem::GoToElevation(units::degree_t angle)
+{
+  if (angle < 45.0_deg)
+  {
+    m_elevationAngle = 0.0; //TODO
+  
+    m_ElevationPIDController.SetReference(m_elevationAngle, rev::CANSparkBase::ControlType::kPosition);
+  }
+
+
+}
+
+void ShooterSubsystem::Shoot(units::meter_t distance)
+{
+  double overRPM = 500.0;
+  double underRPM = 500.0;
+  double backRPM = 500.0;
+
+  m_OverPIDController.SetReference(overRPM, rev::CANSparkBase::ControlType::kVelocity);
+  m_UnderPIDController.SetReference(underRPM, rev::CANSparkBase::ControlType::kVelocity);
+  m_BackPIDController.SetReference(backRPM, rev::CANSparkBase::ControlType::kVelocity);
+
+  
+}
+
+void ShooterSubsystem::Stop()
+{
+  m_BackWheels.StopMotor();
+  m_OverWheels.StopMotor();
+  m_UnderWheels.StopMotor();
+}
+    
--- src/main/include/subsystems/ShooterSubsystem.h
+++ src/main/include/subsystems/ShooterSubsystem.h
@@ -5,12 +5,50 @@
 #include <frc2/command/SubsystemBase.h>
 #include <frc/DataLogManager.h>
 
+#include <rev/CANSparkFlex.h>
+#include <ctre/phoenix6/CANcoder.hpp>
+#include <rev/CANSparkMax.h>
+
+#include <units/angle.h>
+#include <units/length.h>
+
 class ShooterSubsystem : public frc2::SubsystemBase
 {
   public:
     ShooterSubsystem();
     void Periodic();
+    void GoToElevation(units::degree_t angle);
+    void Shoot(units::meter_t distance);
+    void Stop();
     
   private:
-	  wpi::log::DoubleLogEntry m_log;
+    double m_elevationAngle = 0.0;
+
+    rev::CANSparkMax m_OverWheels;
+    rev::CANSparkMax m_UnderWheels;
+    rev::CANSparkFlex m_BackWheels;
+    rev::CANSparkFlex m_ElevationController; 
+
+
+    rev::SparkPIDController m_OverPIDController = m_OverWheels.GetPIDController();
+    rev::SparkPIDController m_UnderPIDController = m_UnderWheels.GetPIDController();
+    rev::SparkPIDController m_BackPIDController = m_BackWheels.GetPIDController();
+    rev::SparkPIDController m_ElevationPIDController = m_ElevationController.GetPIDController();
+
+    rev::SparkRelativeEncoder m_OverRelativeEnc = m_OverWheels.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor, 42);
+    rev::SparkRelativeEncoder m_UnderRelativeEnc = m_UnderWheels.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor, 42);
+    rev::SparkRelativeEncoder m_BackRelativeEnc = m_BackWheels.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor, 42);
+    rev::SparkRelativeEncoder m_ElevationRelativeEnc = m_ElevationController.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor, 42);    
+    
+
+    ctre::phoenix6::hardware::CANcoder m_ElevationEncoder;
+
+	  wpi::log::DoubleLogEntry m_logOverRPM;
+    wpi::log::DoubleLogEntry m_logUnderRPM;
+	  wpi::log::DoubleLogEntry m_logBackRPM;
+	  wpi::log::DoubleLogEntry m_logCurrentAngle;
+	  wpi::log::DoubleLogEntry m_logCommandedAngle;
+	  wpi::log::DoubleLogEntry m_logAbsoluteAngle;
+
+
 };
