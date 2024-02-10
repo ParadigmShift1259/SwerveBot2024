b03a89fb3c7cf291dff4d413537a5b2f914f89d7
Scott Coursin <scott.coursin@gmail.com>
2024-02-09 20:43:53 -0600

    Shooter PIDs working

--- src/main/cpp/subsystems/ShooterSubsystem.cpp
+++ src/main/cpp/subsystems/ShooterSubsystem.cpp
@@ -14,9 +14,9 @@ ShooterSubsystem::ShooterSubsystem()
   , m_ElevationEncoder(kElevationEncoderCANID)
 #endif
 {
-  m_OverWheels.RestoreFactoryDefaults();
-  m_UnderWheels.RestoreFactoryDefaults();
-  m_BackWheels.RestoreFactoryDefaults();
+  //m_OverWheels.RestoreFactoryDefaults();
+  //m_UnderWheels.RestoreFactoryDefaults();
+  //m_BackWheels.RestoreFactoryDefaults();
 
   wpi::log::DataLog& log = frc::DataLogManager::GetLog();
 
@@ -38,39 +38,45 @@ ShooterSubsystem::ShooterSubsystem()
   m_UnderWheels.SetClosedLoopRampRate(0.0);
   m_UnderWheels.SetInverted(false);
 
-  frc::Preferences::InitDouble("kElevationP", 1.0);
+  frc::Preferences::InitDouble("kElevationP", 0.0005);
   frc::Preferences::InitDouble("kElevationI", 0.0);
   frc::Preferences::InitDouble("kElevationD", 0.0);
 #endif
-  frc::Preferences::InitDouble("kShooterP", 1.0);
+  frc::Preferences::InitDouble("kShooterP", 0.0005);
   frc::Preferences::InitDouble("kShooterI", 0.0);
   frc::Preferences::InitDouble("kShooterD", 0.0);
+  frc::Preferences::InitDouble("kShooterFF", 0.0001);
 
-  m_OverPIDController.SetP(frc::Preferences::GetDouble("kShooterP", 1.0));
+  frc::Preferences::InitDouble("kShootVortexP", 0.0009);
+  frc::Preferences::InitDouble("kShootVortexI", 0.0);
+  frc::Preferences::InitDouble("kShootVortexD", 0.0);
+  frc::Preferences::InitDouble("kShootVortexFF", 0.0001);
+
+  m_OverPIDController.SetP(frc::Preferences::GetDouble("kShooterP", 0.0005));
   m_OverPIDController.SetI(frc::Preferences::GetDouble("kShooterI", 0.0));
   m_OverPIDController.SetD(frc::Preferences::GetDouble("kShooterD", 0.0));
-  m_OverPIDController.SetOutputRange(kOverMinOut, kOverMaxOut);
+  m_OverPIDController.SetOutputRange(kMinOut, kMaxOut);
 
-  m_UnderPIDController.SetP(frc::Preferences::GetDouble("kShooterP", 1.0));
+  m_UnderPIDController.SetP(frc::Preferences::GetDouble("kShooterP", 0.0005));
   m_UnderPIDController.SetI(frc::Preferences::GetDouble("kShooterI", 0.0));
   m_UnderPIDController.SetD(frc::Preferences::GetDouble("kShooterD", 0.0));
-  m_UnderPIDController.SetOutputRange(kUnderMinOut, kUnderMaxOut);
+  m_UnderPIDController.SetOutputRange(kMinOut, kMaxOut);
 
 #ifdef OVERUNDER
-  m_BackPIDController.SetP(frc::Preferences::GetDouble("kShooterP", 1.0));
-  m_BackPIDController.SetI(frc::Preferences::GetDouble("kShooterI", 0.0));
-  m_BackPIDController.SetD(frc::Preferences::GetDouble("kShooterD", 0.0));
-  m_BackPIDController.SetOutputRange(kBackMinOut, kBackMaxOut);
+  m_BackPIDController.SetP(frc::Preferences::GetDouble("kShootVortexP", 0.0009));
+  m_BackPIDController.SetI(frc::Preferences::GetDouble("kShootVortexI", 0.0));
+  m_BackPIDController.SetD(frc::Preferences::GetDouble("kShootVortexD", 0.0));
+  m_BackPIDController.SetOutputRange(kMinOut, kMaxOut);
 
-  m_ElevationPIDController.SetP(frc::Preferences::GetDouble("kElevationP", 1.0));
+  m_ElevationPIDController.SetP(frc::Preferences::GetDouble("kElevationP", 0.0005));
   m_ElevationPIDController.SetI(frc::Preferences::GetDouble("kElevationI", 0.0));
   m_ElevationPIDController.SetD(frc::Preferences::GetDouble("kElevationD", 0.0));
   m_ElevationPIDController.SetOutputRange(kMinOut, kMaxOut);
 #endif
 
-  frc::SmartDashboard::PutNumber("OverRPM", 100.0);
-  frc::SmartDashboard::PutNumber("UnderRPM", -100.0);
-  frc::SmartDashboard::PutNumber("BackRPM", -100.0);
+  frc::SmartDashboard::PutNumber("OverRPM", -3000.0);
+  frc::SmartDashboard::PutNumber("UnderRPM", 3000.0);
+  frc::SmartDashboard::PutNumber("BackRPM", -3000.0);
 }
 
 void ShooterSubsystem::Periodic()
@@ -85,9 +91,9 @@ void ShooterSubsystem::Periodic()
   m_logAbsoluteAngle.Append(m_ElevationEncoder.GetAbsolutePosition().GetValueAsDouble());
 #endif
 
-  m_overRPM = frc::SmartDashboard::GetNumber("OverRPM", 100.0);
-  m_underRPM = frc::SmartDashboard::GetNumber("UnderRPM", -100.0);
-  m_backRPM = frc::SmartDashboard::GetNumber("BackRPM", -100.0);
+  m_overRPM = frc::SmartDashboard::GetNumber("OverRPM", -3000.0);
+  m_underRPM = frc::SmartDashboard::GetNumber("UnderRPM", 3000.0);
+  m_backRPM = frc::SmartDashboard::GetNumber("BackRPM", -3000.0);
 
   frc::SmartDashboard::PutNumber("OverRPM echo", m_OverRelativeEnc.GetVelocity());
   frc::SmartDashboard::PutNumber("UnderRPM echo", m_UnderRelativeEnc.GetVelocity());
@@ -110,7 +116,6 @@ void ShooterSubsystem::Shoot(units::meter_t distance)
 {
   //m_OverPIDController.SetIAccum(0);
   //m_UnderPIDController.SetIAccum(0);
-  //static bool bFirstTime = false;
   
 //   double overRPM = 100.0;
 //   double underRPM = -100.0;
@@ -118,19 +123,21 @@ void ShooterSubsystem::Shoot(units::meter_t distance)
 //   double backRPM = -100.0;
 // #endif
 
+  double ffNeo = frc::Preferences::GetDouble("kShooterFF", 0.0001);
+  double ffVtx = frc::Preferences::GetDouble("kShootVortexFF", 0.0001);
+  m_OverPIDController.SetFF(ffNeo);
+  m_UnderPIDController.SetFF(ffNeo);
+
   printf("over %.3f under %.3f back %.3f \n", m_overRPM, m_underRPM, m_backRPM);
   //m_OverWheels.Set(0.5);
   //m_UnderWheels.Set(-0.5);
-  m_OverPIDController.SetReference(m_overRPM, rev::CANSparkBase::ControlType::kVelocity, 0, 0.0, rev::SparkMaxPIDController::ArbFFUnits::kVoltage);
-  m_UnderPIDController.SetReference(m_underRPM, rev::CANSparkBase::ControlType::kVelocity, 0, 0.0, rev::SparkMaxPIDController::ArbFFUnits::kVoltage);
+  m_OverPIDController.SetReference(m_overRPM, rev::CANSparkBase::ControlType::kVelocity);//, 0, ffNeo, rev::SparkMaxPIDController::ArbFFUnits::kVoltage);
+  m_UnderPIDController.SetReference(m_underRPM, rev::CANSparkBase::ControlType::kVelocity);//, 0, ffNeo, rev::SparkMaxPIDController::ArbFFUnits::kVoltage);
 #ifdef OVERUNDER  
-  //m_BackWheels.Set(-0.5);
-  //if (!bFirstTime)
-  {
-    m_BackPIDController.SetReference(m_backRPM, rev::CANSparkBase::ControlType::kVelocity, 0, 0.0, rev::SparkMaxPIDController::ArbFFUnits::kVoltage);
-  }
+  m_BackPIDController.SetFF(ffVtx);
+  //m_BackWheels.Set(m_backRPM);
+  m_BackPIDController.SetReference(m_backRPM, rev::CANSparkBase::ControlType::kVelocity);//, 0, ffVtx, rev::SparkMaxPIDController::ArbFFUnits::kVoltage);
 #endif
-  //bFirstTime = true;
 }
 
 void ShooterSubsystem::Stop()
--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -5,13 +5,4 @@
 constexpr double kMinOut = -1.0;
 constexpr double kMaxOut = 1.0;
 
-constexpr double kOverMinOut = -1000.0;
-constexpr double kOverMaxOut = 1000.0;
-
-constexpr double kUnderMinOut = -1000.0;
-constexpr double kUnderMaxOut = 0.0;
-
-constexpr double kBackMinOut = -1000.0;
-constexpr double kBackMaxOut = 0.0;
-
 constexpr double kIntakeDeployGearRatio = 22.0 / 36.0;  // 22T sprocket input, 36T sprocket output
