2c791a220385ca12b0b9272781e5c4b725eb49a8
Scott Coursin <scott.coursin@gmail.com>
2024-03-15 11:26:50 -0500

    Set Intake deploy to 49

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -12,8 +12,8 @@ constexpr double kDeployMaxOut = 0.5;
 
 constexpr double kIntakeDeployGearRatio = 22.0 / 36.0;  // 22T sprocket input, 36T sprocket output
 
-constexpr double c_defaultRetractTurns = 4.0;//-0.5;
-constexpr double c_defaultExtendTurns = 47.5;//43.0;
+constexpr double c_defaultRetractTurns = 4.0;
+constexpr double c_defaultExtendTurns = 49;
 
 constexpr units::angle::degree_t c_defaultTravelPosition = 33.0_deg;
 constexpr units::angle::degree_t c_defaultStartPosition = 66.0_deg;
